1c3a1e10895b4168a5842d12bb66bc2d717dfa24
vitoriasmedeiros <vitoriasmedeiros@github.com>
2019-12-19 15:58:47 -0300

    alteracoes de hoje

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const tarefas = [
+  "Aula de componentes",
+  "Aula sobre propriedades e estados",
+  "Outra aula sobre UI"
+];
+
+class TarefaLista extends React.Component {
+  render() {
+    const titulo = this.props.title;
+    const dados = this.props.data;
+    const {title, data} = this.props;
+    console.log(this.props)
+    return (
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {title}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        <ul>
+          {data.map((item) => <li>{item}</li>)}
+        </ul>
+      </div>
+    );
+  }
 }
+
+const Tarefa = (propriedades) => {
+  return (<div>
+    <p>{propriedades.title}</p>
+    <input type="text"></input>
+    <button type="submit">Criar nova tarefa</button>
+  </div>);
+}
+
+class App extends React.Component {
+  state = {tarefas:[
+    "Aula de componentes",
+    "Aula sobre propriedades e estados",
+    "Outra aula sobre UI"
+  ],
+  text: "Minha nova terefa"
+};
+
+    onChanged
+    render(){
+      return(
+        <div>
+          <TarefaLista data={this.state.tarefas} tittle = "Minhas terefas"></TarefaLista>
+          <Tarefa title="minha nova tarefa" text={this.state.tarefas} onChanged={(text)}></Tarefa>       
+        </div>
+         );
+      }
+    }
+
+
 
 export default App;
